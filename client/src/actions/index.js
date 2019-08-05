e1b95ae0c3b1886a2eb9c7f5357f64a06e868581
Jonathan Vega <jonnyv212@hotmail.com>
2019-08-05 19:25:55 -0400

    commit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,16 +1,5 @@
 import axios from "axios";
 
-// export const search = searchData => {
-//   return dispatch => {
-//     setTimeout(() => {
-//       dispatch({
-//         type: "SEARCH",
-//         payload: searchData
-//       });
-//     }, 2000);
-//   };
-// };
-
 export const search = () => {
   return dispatch => {
     return axios.get("/zipcodeSearch/48197").then(response => {
